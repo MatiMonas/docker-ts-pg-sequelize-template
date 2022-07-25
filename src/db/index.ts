bee15f19573ab9f8b34a1ce075a42b43b4e9ad74
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-25 17:58:56 -0300

    add optional number in port

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: DB_HOST || 'postgres',
-  port: Number(DB_PORT),
+  port: Number(DB_PORT) || 5432,
   database: DB_NAME || 'postgres',
   username: DB_USERNAME || 'postgres',
   password: DB_PASSWORD || 'postgres',
