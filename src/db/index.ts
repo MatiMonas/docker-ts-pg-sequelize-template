6b229e1c8dd6dcd484e1ff85c84e9fbfafde26f2
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-25 17:58:25 -0300

    update new Sequelize

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,14 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: DB_HOST || "postgres",
+  dialect: 'postgres',
+  host: DB_HOST || 'postgres',
   port: Number(DB_PORT),
-  database: DB_NAME || "postgres",
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
+  database: DB_NAME || 'postgres',
+  username: DB_USERNAME || 'postgres',
+  password: DB_PASSWORD || 'postgres',
   define: {
     underscored: true,
   },
