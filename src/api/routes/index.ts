ed77950657a7da55ad1a01647963b6a377c99ae5
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-26 18:35:50 -0300

    add(api/routes) countries router

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import countryRouter from './countries';
 import helloWorldRouter from './helloWorld'
 
 const router = Router()
 
-.use('/', helloWorldRouter);
+.use('/', helloWorldRouter)
+.use('/countries', countryRouter)
 
 export default router;
