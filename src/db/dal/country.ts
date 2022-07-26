75d43ed9f12857f1db16eab57f10fbe1ef6c7036
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-26 18:37:11 -0300

    add(db/dal/country)  custom error and validations

--- src/db/dal/country.ts
+++ src/db/dal/country.ts
@@ -1,10 +1,23 @@
 import { Op } from 'sequelize';
+import { CustomError } from '../../common/errors';
 import Country from '../models/Country.model';
 import { CountryInput, CountryOutput } from '../models/Country.model';
 import { GetAllCountriesFilters } from './types';
 
 export const create = async (payload: CountryInput): Promise<CountryOutput> => {
-  const country = await Country.create(payload);
+  const checkCountry = await Country.findOne({
+    where: {
+      name: {
+        [Op.eq]: payload.name,
+      },
+    },
+  })
+  
+  if(checkCountry) {
+    throw new CustomError('Country already exists', 400);
+  }
+
+  const country = await Country.create(payload);1
   return country.toJSON() as CountryOutput;
 };
 
@@ -21,12 +34,12 @@ export const findOrCreate = async (
 
 export const update = async (
   id: number,
-  payload: CountryInput,
+  payload: Partial<CountryInput>,
 ): Promise<CountryOutput> => {
   const country = await Country.findByPk(id);
 
   if (!country) {
-    throw new Error('Country not found');
+    throw new CustomError('Country not found', 404);
   }
 
   const updatedCountry = await country.update(payload);
@@ -37,7 +50,8 @@ export const getById = async (id: number): Promise<CountryOutput> => {
   const country = await Country.findByPk(id);
 
   if (!country) {
-    throw new Error('Country not found');
+    throw new CustomError('Country not found', 404);
+
   }
   return country.toJSON() as CountryOutput;
 };
@@ -58,5 +72,9 @@ export const getAll = async (filters?: GetAllCountriesFilters): Promise<CountryO
     ...((filters?.isDeleted || filters?.includeDeleted) && { paranoid: true }),
   });
 
+  if(!countries) {
+    throw new CustomError('No conuntries found', 404);
+  }
+  
   return countries.map((country) => country.toJSON()) as CountryOutput[];
 };
