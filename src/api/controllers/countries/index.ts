ba43e8bb53391b4de9ce0ee38d549cfc66de6083
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-26 20:01:55 -0300

    changes(controllers) services name

--- src/api/controllers/countries/index.ts
+++ src/api/controllers/countries/index.ts
@@ -1,26 +1,26 @@
-import * as service from '../../../db/services/CountryService';
+import * as countryService from '../../../db/services/CountryService';
 import { CreateCountryDTO, FilterCountriesDTO, UpdateCountryDTO } from '../../dto/country.dto';
 
 import { Country } from '../../interfaces';
 import * as mapper from './mapper';
 
 export const create = async (payload: CreateCountryDTO): Promise<Country> => {
-  return mapper.toCountry(await service.create(payload));
+  return mapper.toCountry(await countryService.create(payload));
 };
 
 export const update = async (
   id: number,
   payload: UpdateCountryDTO,
 ): Promise<Country> => {
-  return mapper.toCountry(await service.update(id, payload));
+  return mapper.toCountry(await countryService.update(id, payload));
 };
 
 export const getById = async (id: number): Promise<Country> => {
-  return mapper.toCountry(await service.getById(id));
+  return mapper.toCountry(await countryService.getById(id));
 };
 
 export const deleteById = async (id: number): Promise<Boolean> => {
-  const isDeleted = await service.deleteById(id);
+  const isDeleted = await countryService.deleteById(id);
 
   return isDeleted;
 };
@@ -28,5 +28,5 @@ export const deleteById = async (id: number): Promise<Boolean> => {
 export const getAll = async (
   filters: FilterCountriesDTO,
 ): Promise<Country[]> => {
-  return (await service.getAll(filters)).map(mapper.toCountry);
+  return (await countryService.getAll(filters)).map(mapper.toCountry);
 };
--- src/api/controllers/users/index.ts
+++ src/api/controllers/users/index.ts
@@ -1,26 +1,26 @@
-import * as service from '../../../db/services/UserService';
+import * as userService from '../../../db/services/UserService';
 import { CreateUserDTO, FilterUsersDTO, UpdateUserDTO } from '../../dto/user.dto';
 
 import { User } from '../../interfaces';
 import * as mapper from './mapper';
 
 export const create = async (payload: CreateUserDTO): Promise<User> => {
-  return mapper.toUser(await service.create(payload));
+  return mapper.toUser(await userService.create(payload));
 };
 
 export const update = async (
   id: number,
   payload: UpdateUserDTO,
 ): Promise<User> => {
-  return mapper.toUser(await service.update(id, payload));
+  return mapper.toUser(await userService.update(id, payload));
 };
 
 export const getById = async (id: number): Promise<User> => {
-  return mapper.toUser(await service.getById(id));
+  return mapper.toUser(await userService.getById(id));
 };
 
 export const deleteById = async (id: number): Promise<Boolean> => {
-  const isDeleted = await service.deleteById(id);
+  const isDeleted = await userService.deleteById(id);
 
   return isDeleted;
 };
@@ -28,5 +28,5 @@ export const deleteById = async (id: number): Promise<Boolean> => {
 export const getAll = async (
   filters: FilterUsersDTO,
 ): Promise<User[]> => {
-  return (await service.getAll(filters)).map(mapper.toUser);
+  return (await userService.getAll(filters)).map(mapper.toUser);
 };
