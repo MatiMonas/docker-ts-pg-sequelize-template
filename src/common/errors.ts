e00b19dd275b15242bd5c19b7bd80abae5b275aa
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-26 18:36:06 -0300

    add(common/errors) custom error class

---
+++ src/common/errors.ts
@@ -0,0 +1,11 @@
+export class CustomError extends Error {
+  public status: number;
+  public message: string;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
